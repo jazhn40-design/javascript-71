d76dbd10573c6a4c3881e46b1bd93bf26d8b4240
jaz <jazhn40@gmail.com>
2026-08-26 14:02:05 -0600

    Se modifico el codigo del archivo app.js

--- pf-js/app.js
+++ pf-js/app.js
@@ -18,3 +18,13 @@ do{
 
 }while (counter <= 100);
 console.log(counter);
+
+counter= 1;
+do{
+    if (counter % 2 == 0){
+        console.log(`El numero ${counter} es par`);
+    } else{
+        console.log(`El numero ${counter} es impar`);
+    }
+    counter ++;
+} while (counter < 200);
